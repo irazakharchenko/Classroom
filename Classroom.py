dbc609f72e6b70b4f84ce6c9db322eb9b9589ba1
irazakharchenko <zakharchenko@ucu.edu.ua>
2017-02-15 20:55:19 +0200

    add modules __str__, equipment_differences

--- Classroom.py
+++ Classroom.py
@@ -10,6 +10,21 @@ class Classroom:
     def is_larger(self, other):
         return self.capacity > other.capacity
 
-cl1 = Classroom('6', 6, [7,8])
+    def equipment_differences(self, other):
+        return list(set(self.equipment) - set(other.equipment))
+
+    def  __str__ (self):
+        strich =  'Classroom {0} has a capacity of {1}  persons and has the following equipment: '\
+            .format(self.number, self.capacity)
+        for i in self.equipment:
+            strich += i + ', '
+        return strich[:-2] + '.\n'
+    def  __repr__ (self):
+        pass
+
+
+
+
+cl1 = Classroom('6', 6, ['dsd','ddd'])
 cl2 = Classroom('8', 4, [6,7])
-print(cl1.is_larger(cl2))
+print(cl1.__str__())
